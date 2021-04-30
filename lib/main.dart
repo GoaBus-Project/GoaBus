359d2a21d5a279f425c598ed0ae5a73e7ff980ef
DpkPatil05 <patildeepak188@gmail.com>
2021-05-01 00:33:59 +0530

    new project

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:goa_bus/screens/login_screen.dart';
+
+import 'package:goa_bus/providers/login_provider.dart';
+
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => LoginProvider()),
+      ],
+      child: MaterialApp(
+          home: LoginScreen()
+      ),
+    );
+  }
+
+}
---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+          child: Text("First flutter web app"),
+        )
+    );
+  }
+}
