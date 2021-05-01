67d18cbbba7155045e3e02508023881346d06a9b
DpkPatil05 <patildeepak188@gmail.com>
2021-05-01 14:50:31 +0530

    login page UI complete

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,4 +1,6 @@
 class Constants {
   Constants._();
 
+  static const String MAIN_LOGO = 'logo.png';
+  static const String GOOGLE_LOGO = 'google-logo.png';
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:goa_bus/constants/color_palette.dart';
+import 'package:goa_bus/constants/constants.dart';
+import 'package:goa_bus/providers/login_provider.dart';
+import 'package:provider/provider.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -11,47 +14,194 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          Expanded(
-            flex: 3,
-            child: Container(
-              color: Palette.primary,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(
-                          'logo.png',
-                          height: 200,
-                          width: 200,
+        body: Row(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                  color: Palette.primary,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset(
+                              Constants.MAIN_LOGO,
+                              height: 200,
+                              width: 200,
+                            ),
+                            Text(
+                              "Goa Bus".toUpperCase(),
+                              style: TextStyle(
+                                color: Palette.fontColor,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 60,
+                              ),
+                            ),
+                          ],
                         ),
-                        Text(
-                          'Goa Bus'.toUpperCase(),
-                          style: TextStyle(
-                            color: Palette.fontColor,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 50,
+                      )
+                    ],
+                  )
+              ),
+            ),
+            Consumer<LoginProvider>(
+              builder: (context, loginProv, _) {
+                loginProv.setGreeting();
+                return Expanded(
+                    flex: 2,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Container(
+                              width: 180,
+                              decoration: BoxDecoration(
+                                border: Border(
+                                  bottom: BorderSide(
+                                      width: 3, color: Palette.primary),
+                                ),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.only(bottom: 8),
+                                child: Text(
+                                  "Login".toUpperCase(),
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Palette.primary,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 60),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  "${loginProv.greet},",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 35,
+                                  ),
+                                ),
+                                Text(
+                                  "Admin",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 35,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            SizedBox(height: 60),
+                            ElevatedButton(
+                              style: ButtonStyle(
+                                backgroundColor: MaterialStateProperty.all<Color>(Colors.white),
+                              ),
+                              onPressed: () {  },
+                              child: SizedBox(
+                                height: 40,
+                                width: 300,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    Image.asset(
+                                      Constants.GOOGLE_LOGO,
+                                      height: 40,
+                                      width: 40,
+                                    ),
+                                    Text(
+                                      "Sign in with Google",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            SizedBox(height: 60),
+                            Text(
+                              "or",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.black,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          width: 300,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                decoration: InputDecoration(
+                                    border: UnderlineInputBorder(),
+                                    labelText: 'Enter your email'
+                                ),
+                              ),
+                              SizedBox(height: 15),
+                              TextFormField(
+                                decoration: InputDecoration(
+                                    border: UnderlineInputBorder(),
+                                    labelText: 'Enter your password'
+                                ),
+                              ),
+                              SizedBox(height: 15),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  ElevatedButton(
+                                    style: ButtonStyle(
+                                      backgroundColor: MaterialStateProperty.all<Color>(Palette.primary),
+                                    ),
+                                    onPressed: () {  },
+                                    child: Padding(
+                                      padding: const EdgeInsets.only(left: 15, right: 15, top: 11, bottom: 11),
+                                      child: Text(
+                                        "Login".toUpperCase(),
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          color: Palette.fontColor,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  TextButton(
+                                    onPressed: () {},
+                                    child: Text(
+                                      "Forgot Password",
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: Colors.black,
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              )
+                            ],
                           ),
                         ),
                       ],
                     ),
-                  )
-                ],
-              )
-            ),
-          ),
-          Expanded(
-              flex: 2,
-              child: Column(
-              children: [],
+                );
+              }
             )
-          )
-        ],
-      ),
+          ],
+        ),
     );
   }
 }
