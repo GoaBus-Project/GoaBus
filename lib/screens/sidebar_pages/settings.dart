1ef2e8590ed0aacf7f1ed76ebbc4ac9b0c48964a
vishaal <vishnair001@gmail.com>
2021-05-04 21:08:00 +0530

    ui changes

--- lib/screens/sidebar_pages/settings.dart
+++ lib/screens/sidebar_pages/settings.dart
@@ -7,6 +7,8 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
+  final _formKey = GlobalKey<FormState>();
+  final textEditingController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -19,9 +21,89 @@ class _SettingsState extends State<Settings> {
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 FloatingActionButton.extended(
-                  onPressed: () {},
                   label: Text("Change Password",style: TextStyle(color: Colors.white),),
                   backgroundColor: Palette.buttonColor,
+                  onPressed: () {
+                    showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            content: Stack(
+                              clipBehavior: Clip.none,
+                              children: <Widget>[
+                                Positioned(
+                                  right: -40.0,
+                                  top: -40.0,
+                                  child: InkResponse(
+                                    splashColor: Palette.primary,
+                                    onTap: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: CircleAvatar(
+                                      child: Icon(Icons.close),
+                                      backgroundColor: Palette.buttonColor,
+                                    ),
+                                  ),
+                                ),
+                              Form(
+                              key: _formKey,
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  // TextFormField(
+                                  //   validator: (value) {
+                                  //     if (value == null || value.isEmpty) {
+                                  //       return 'Please enter some text';
+                                  //     }
+                                  //     return null;
+                                  //   },
+                                  //   decoration: InputDecoration(
+                                  //     labelText: 'Current Password',
+                                  //     icon: Icon(Icons.vpn_key_outlined),
+                                  //   ),
+                                  // ),
+                                  TextFormField(
+                                    decoration: const InputDecoration(
+                                      icon: const Icon(Icons.vpn_key_outlined),
+                                      labelText: 'Current Password',
+                                    ),
+                                    controller: textEditingController,
+                                  ),
+                                  TextFormField(
+                                    decoration: const InputDecoration(
+                                      icon: const Icon(Icons.vpn_key_outlined),
+                                      labelText: 'New Password',
+                                    ),
+                                    controller: textEditingController,
+                                  ),
+                                  TextFormField(
+                                    decoration: const InputDecoration(
+                                      icon: const Icon(Icons.vpn_key_outlined),
+                                      labelText: 'Confirm new Password',
+                                    ),
+                                    controller: textEditingController,
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                                    child: ElevatedButton(
+                                      onPressed: () {
+                                        if (_formKey.currentState.validate()) {
+                                          ScaffoldMessenger.of(context)
+                                              .showSnackBar(SnackBar(content: Text('Password changed')));
+                                        }
+                                      },
+                                      child: Text('Submit'),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                              ],
+                            ),
+                          );
+                        });
+                  },
                 ),
               ],
             ),
@@ -30,9 +112,9 @@ class _SettingsState extends State<Settings> {
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 FloatingActionButton.extended(
-                  onPressed: () {},
                   label: Text("Organisation Settings",style: TextStyle(color: Colors.white),),
                   backgroundColor: Palette.buttonColor,
+                  onPressed: () {},
                 ),
               ],
             ),
@@ -41,9 +123,9 @@ class _SettingsState extends State<Settings> {
               mainAxisAlignment: MainAxisAlignment.start,
               children: [
                 FloatingActionButton.extended(
-                  onPressed: () {},
                   label: Text("About",style: TextStyle(color: Colors.white),),
                   backgroundColor: Palette.buttonColor,
+                  onPressed: () {},
                 ),
               ],
             ),
