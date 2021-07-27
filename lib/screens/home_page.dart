f3a45a97bb3b962e8ab0f9b54055e51c345122ad
Deepak Patil <patildeepak188@gmail.com>
2021-07-27 22:43:52 +0530

    polylines route path fixes

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -38,15 +38,13 @@ class _HomePageState extends State<HomePage> {
 
   Future<void> fetchBusLocation(Bus bus) async {
     final prov = Provider.of<HomeProvider>(context, listen: false);
+    bus = await prov.fetchBus(bus);
+    prov.startEndPoints.clear();
     prov.startEndPoints.add(LatLng(bus.lat, bus.lng));
     prov.startEndPoints.add(prov.destinationBusStop);
     await prov.getPolyline();
     timer = Timer.periodic(Duration(seconds: 5), (Timer t) async {
       bus = await prov.fetchBus(bus);
-      prov.startEndPoints.clear();
-      prov.startEndPoints.add(LatLng(bus.lat, bus.lng));
-      prov.startEndPoints.add(prov.destinationBusStop);
-      await prov.getPolyline();
       _googleMapController.animateCamera(CameraUpdate.newCameraPosition(
           CameraPosition(
             target: LatLng(bus.lat, bus.lng),
